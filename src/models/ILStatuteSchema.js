79c618744f69e9a76fb1b1f1834047222a2608ca
Jason Sherrick <jasonsherrick@gmail.com>
2023-12-26 00:42:26 -0600

    updated statute schemas

--- src/models/ILStatuteSchema.js
+++ src/models/ILStatuteSchema.js
@@ -1,6 +1,18 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
+const ActSchema = new Schema({
+  number: Number,
+  title: String,
+  text: String,
+});
+
+const ChapterSchema = new Schema({
+  number: Number,
+  title: String,
+  acts: [ActSchema],
+});
+
 const ILStatuteSchema = new Schema({
   chapter: String,
   act: String,
