e78bdd0579f378eedd7f530532b3710b24cd46f9
Jason Sherrick <jasonsherrick@gmail.com>
2023-12-29 22:30:30 -0600

    added function to get list of links

--- src/utils/scraping/ilgaScraper.js
+++ src/utils/scraping/ilgaScraper.js
@@ -2,20 +2,32 @@ import puppeteer, { Page } from 'puppeteer';
 
 
 /**
- * 
- * @param { Page } page is from `ilga.gov/legislation/ilcs/ilcs.asp` or any subpages
+ * @param { Page } page `ilga.gov/legislation/ilcs/ilcs.asp` or any subpages
  *  
  * @returns { Array } of strings representing an index of chapters or acts on the page
  */
-async function getPageIndex(page) {
+async function getArrayOfChapters(page) {
     let index = await page.$eval('td ul', el => el.innerText);
     return index.split('\n').filter((el) => el !== '');
 }
 
+/**
+ * @param { Page } page `ilga.gov/legislation/ilcs/ilcs.asp` or any subpages listing chapters or acts
+ * 
+ * @returns { Array } of strings representing all the topical links on a ilga ilcs page
+ */
+async function getArrayOfLinks(page) {
+    let links = await page.$$eval('td ul a', (els) => els.map((el) => {
+        return {
+            text: el.innerText,
+            href: el.href
+        }
+    }));
+    return links;
+}
 
 /**
- * 
- * @param { Array } index in general is the index returned from getPageIndex(),
+ * @param { Array } index is the index returned from getPageIndex(),
  * but can be any array of strings formatted the same way as on the ilga website
  * 
  * @returns { Array } filtered to just the major topics of legislation 
@@ -26,8 +38,7 @@ function getMajorTopics(index) {
 
 
 /**
- * 
- * @param { Array } index in general is the index returned from getPageIndex(),
+ * @param { Array } index is the index returned from getPageIndex(),
  * @returns { Array } filtered to just chapter numbers
  */
 function getChapterNumbers(index) {
@@ -38,14 +49,13 @@ function getChapterNumbers(index) {
 
 
 /**
- * 
- * @param { Array } index in general is the index returned from getPageIndex(),
+ * @param { Array } index is the index returned from getPageIndex(),
  * @returns { Array } filtered to just the chapter topics
  */
 function getChapterTopics(index) {
     let chapters = index.filter((el) => el.includes('CHAPTER'));
 
-    // this feels like a shitshow and I should definitely come tidy this up later
+    // there should be a more concise way to do this
     chapters = chapters.map((el) => el.replace('CHAPTER', '').trim().substring(4).trim() );
 
     // here's some other ways I tried that didn't work
